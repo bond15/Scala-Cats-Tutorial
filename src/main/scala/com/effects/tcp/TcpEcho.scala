d585de581c87609cdd7f2ae23dd3a27a7715f7db
bond15 <bond15@purdue.edu>
2020-01-17 14:03:25 -0800

    Added server STOP command to gracefully stop server

--- src/main/scala/com/effects/tcp/TcpEcho.scala
+++ src/main/scala/com/effects/tcp/TcpEcho.scala
@@ -1,6 +1,7 @@
 package com.effects.tcp
 
 import cats.effect._
+import cats.effect.concurrent.MVar
 import cats.effect.syntax.all._
 import cats.effect.ExitCase._
 import cats.implicits._
@@ -14,20 +15,21 @@ object TcpEcho extends IOApp {
     def close[F[_]: Sync](socket: ServerSocket): F[Unit] =
       Sync[F].delay(socket.close()).handleErrorWith(_ => Sync[F].unit)
 
-    IO( new ServerSocket(args.headOption.map(_.toInt).getOrElse(5431)))
+    IO( new ServerSocket(args.headOption.map(_.toInt).getOrElse(5433)))
       .bracket {
-        serverSocket => serve[IO](serverSocket) >> IO.pure(ExitCode.Success)
+        serverSocket => server[IO](serverSocket) >> IO.pure(ExitCode.Success)
       } {
         serverSocket => close[IO](serverSocket) >> IO(println("Server finished"))
       }
 
   }
 
-  def echoProtocol[F[_]: Sync](clientSocket: Socket): F[Unit] = {
+  def echoProtocol[F[_]: Sync](clientSocket: Socket, stopFlag: MVar[F,Unit]): F[Unit] = {
 
     def loop(reader: BufferedReader, writer: BufferedWriter): F[Unit] = for {
       line <- Sync[F].delay(reader.readLine())
       _ <- line match {
+        case "STOP" => stopFlag.put(())
         case "" => Sync[F].unit
         case _ => Sync[F].delay {
           writer.write(line); writer.newLine(); writer.flush()
@@ -60,7 +62,7 @@ object TcpEcho extends IOApp {
     }
   }
 
-  def serve[F[_]: Concurrent](serverSocket: ServerSocket): F[Unit] = {
+  def serve[F[_]: Concurrent](serverSocket: ServerSocket, stopFlag: MVar[F,Unit]): F[Unit] = {
 
     def close(socket: Socket): F[Unit] =
       Sync[F].delay(socket.close()).handleErrorWith(_ => Sync[F].unit)
@@ -68,18 +70,25 @@ object TcpEcho extends IOApp {
     for {
       _ <- Sync[F].delay(serverSocket.accept())
         .bracketCase {socket =>
-          echoProtocol(socket).guarantee(close(socket))//.start // 'start' run on own fiber
+          echoProtocol(socket,stopFlag).guarantee(close(socket)).start // 'start' run on own fiber
         } { (socket,exit) =>
           exit match {
             case Completed => Sync[F].unit
             case Error(_) | Canceled => close(socket)
           }
         }
-      _ <- serve(serverSocket)
+      _ <- serve(serverSocket,stopFlag)
     }  yield ()
 
   }
 
+  def server[F[_]: Concurrent](serverSocket: ServerSocket): F[ExitCode] =
+    for {
+      stopFlag <- MVar[F].empty[Unit]
+      serverFiber <- serve(serverSocket, stopFlag).start // server runs on own fiber
+      _ <- stopFlag.read // blocked until 'stopFlag.put(())' is run
+      _ <- serverFiber.cancel.start // Stopping server (on another fiber)
+    } yield ExitCode.Success
 
 
 }
